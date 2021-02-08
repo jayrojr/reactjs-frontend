236b15f352913d4766afea926ee40673121eb1f1
jayro <jayro@pop-os.localdomain>
2021-02-08 19:14:07 -0300

    Listando Projetos da API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
 import backgroundImage from './assets/background.jpeg';
+import api from './services/api';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      console.log(response);
+      setProjects(response.data);
+    })
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
-
     console.log(projects);
   }
 
   return (
     <>
       <Header title="Projects"/>
-      <img width={300} src={backgroundImage}/>
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
       <ul>
         {
           projects.map(project => (
-            <li key={project}>{project}</li>
-          ))
-        }
+            <li key={project.id}>{project.title}</li>
+            ))
+          }
       </ul>
-
+      <img width={300} src={backgroundImage}/>
     </>
   );
 }
